93d69da7ca8ec80823a490d15b55d07e3b79840f
ganesh-nallamilli <nunganesh46@gmail.com>
2024-12-02 16:31:25 +0530

    minor changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -73,6 +73,7 @@ const Header: React.FC = () => {
               type="text"
               placeholder="Search..."
               className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
+              id="global_search"
             />
             <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
           </div>
